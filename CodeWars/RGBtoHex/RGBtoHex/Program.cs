757869e89102ef1159eb4ed50c3868b9b8879ea3
Kevin Petow <kevin.m.petow@outlook.com>
2021-12-26 12:38:50 -0500

    added RGB to Hex problem from Code Wars

---
+++ CodeWars/RGBtoHex/RGBtoHex/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RGBtoHex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int r = 148;
+            int g = 0;
+            int b = 211;
+            string Hex = "";
+            
+            Dictionary<int, char> hex = new Dictionary<int, char>();
+            #region
+            hex.Add(0, '0');
+            hex.Add(1, '1');
+            hex.Add(2, '2');
+            hex.Add(3, '3');
+            hex.Add(4, '4');
+            hex.Add(5, '5');
+            hex.Add(6, '6');
+            hex.Add(7, '7');
+            hex.Add(8, '8');
+            hex.Add(9, '9');
+            hex.Add(10, 'A');
+            hex.Add(11, 'B');
+            hex.Add(12, 'C');
+            hex.Add(13, 'D');
+            hex.Add(14, 'E');
+            hex.Add(15, 'F');
+            #endregion
+
+            int[] input = { r, g, b };
+
+            for(int i = 0; i < input.Length; i++)
+            {
+                int hold = input[i];
+                if (hold < 0) {hold = 0;}
+                if (hold > 255) {hold = 255;}
+
+                int aux1 = hold / 16;
+                int aux2 = hold % 16;
+
+                Hex += hex[aux1].ToString() + hex[aux2].ToString();
+                
+            }
+            Console.WriteLine(Hex);
+        }
+    }
+}
