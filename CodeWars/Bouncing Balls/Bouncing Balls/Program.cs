f2db30c52f2edd31934b5f9c23c3c0cc17bfc9e1
Kevin Petow <kevin.m.petow@outlook.com>
2021-12-26 11:28:18 -0500

    Added Bouncing Balls problem from Code Wars

---
+++ CodeWars/Bouncing Balls/Bouncing Balls/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bouncing_Balls
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double h = 3.0;
+            double bounce = 0.66;
+            double window = 1.5;
+
+            int count = 0;
+
+            if (h > 0 && bounce > 0 && bounce < 1 && window < h)
+            {
+                while (h > window)
+                {
+                    count++;
+                    h = bounce * h;
+
+                    if (h > window)
+                    {
+                        count++;
+                    }
+
+                    Console.WriteLine($"Count is {count}");
+                    Console.WriteLine($"Height is {h}");
+                }
+                Console.WriteLine(count);
+            }
+            else
+            {
+                Console.WriteLine(-1);
+            }
+        }
+    }
+}
