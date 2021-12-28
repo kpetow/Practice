c607f513d26e63158d719b08d0ed10b45f4be67f
Kevin Petow <kevin.m.petow@outlook.com>
2021-12-27 22:03:10 -0500

    Code Wars Problem: Build Tower

---
+++ CodeWars/Build Tower - 6 kyu/Build Tower - 6 kyu/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Build_Tower___6_kyu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nFloors = 3;
+            string[] result = new string[nFloors];
+
+            int starsN = 0;
+            int spacesN = 0;
+            string stars = "";
+            string spaces = "";
+
+            for (int f = 1; f <= nFloors; f++)
+            {
+                starsN = f * 2 - 1;
+                spacesN = (nFloors * 2 - 1 - starsN) / 2;
+                Console.WriteLine(starsN);
+                Console.WriteLine(spacesN);
+
+                for (int j = 1; j <= starsN; j++)
+                {
+                    stars += "*";                    
+                }
+                for (int k = 1; k <= spacesN; k++)
+                {
+                    spaces += " ";
+                }
+                result[f - 1] = spaces + stars + spaces;
+                stars = "";
+                spaces = "";
+            }
+            foreach (string s in result)
+            {
+                Console.Write("|");
+                Console.Write(s);
+                Console.Write("|\n");
+
+            }
+        }
+    }
+}
