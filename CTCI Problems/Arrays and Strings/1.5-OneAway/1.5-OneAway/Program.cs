e839c81dfaa911498a270faecc487e1a91a43784
Kevin Petow <kevin.m.petow@outlook.com>
2021-12-31 16:06:06 -0500

    added folder for 'Cracking the Coding Interview' practice problems; completed Exercise 1.5 - One Away. Also tried some codewars problems (pathfinder 1 and factorial decomposition), but couldn't figure them out.

---
+++ CTCI Problems/Arrays and Strings/1.5-OneAway/1.5-OneAway/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * There are 3 types of edits that can be performed on strings: insert a character, remove a character, or replace a character.
+ * Given 2 strings, write a function to check if they are one edit (or zero edits) away.
+ * 
+ * Example:
+    pale, ple => true
+    pales, pale => true
+    pale, bale => true
+    pale, bake => false
+ */
+
+namespace _1._5_OneAway
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("First string: ");
+            string s1 = Console.ReadLine();
+            Console.Write("Second string: ");
+            string s2 = Console.ReadLine();
+
+            //Length check
+            int diff = Math.Abs(s1.Length - s2.Length);
+
+            if (diff > 1) { Console.WriteLine(false + ": failed on length"); }
+            if (diff == 1) { Console.WriteLine(insertCheck(s1, s2) + ": used insert check"); }
+            if (diff == 0) { Console.WriteLine(replaceCheck(s1, s2) + ": used replace check"); }
+        }        
+
+        //Method to test insertion of characters (strings are different lengths)
+        static bool insertCheck(string a, string b)
+        {
+            string c = string.Empty;
+            string d = string.Empty;
+            if(a.Length > b.Length){c = b; d = a; }
+            else {c = a; d = b;}
+
+            for (int i = 0; i < c.Length; i++)
+            {
+                if(c[i] != d[i] && c[i] != d[i+1])
+                {
+                    return false;
+                }
+            }
+            return true;    
+        }
+
+        //Strings are same length, testing for identical characters
+        static bool replaceCheck(string a, string b)
+        {
+            int count = 0;
+            for (int i = 0;i < a.Length; i++)
+            {
+                if(a[i] != b[i])
+                {
+                    count++;
+                }
+                if(count > 1)
+                {
+                    return false;
+                }
+            }
+            return true ;
+        }
+       
+    }
+}
---
+++ CodeWars/5 kyu/Factorial Decomposition/Factorial Decomposition/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+//had to look up the solution. couldn't figure this one out
+
+static class FactDecomp
+{
+    static void Main(string[] args)
+    {
+        int fact = int.Parse(Console.ReadLine());
+        Console.WriteLine(Decomp(fact));
+    }
+    public static string Decomp(int n)
+    {
+        var result = string.Empty;
+        return Enumerable.Range(2, n - 1)
+          .Select(x => x.Multipliers())
+          .SelectMany(x => x)
+          .GroupBy(x => x)
+          .OrderBy(x => x.Key)
+          .Aggregate(result, (res, x) => res + $" * {x.Key}" + (x.Count() > 1 ? $"^{x.Count()}" : ""))
+          .Trim(' ', '*');
+    }
+
+    public static IEnumerable<int> Multipliers(this int number)
+    {
+        var divider = 2;
+        while (number != 1)
+        {
+            if (number % divider == 0)
+            {
+                number /= divider;
+                yield return divider;
+            }
+            if (number % divider != 0) divider++;
+        }
+    }
+}
